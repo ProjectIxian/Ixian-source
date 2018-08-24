16d9794a8e8119520574217235ff8fd0e6a7aa60
IxiAngel <no-reply@ixian.io>
2018-08-24 01:07:31 +0100

    Added more details (blockChecksum and walletStateChecksum) to debugDumpState

--- IxianDLT/Meta/Node.cs
+++ IxianDLT/Meta/Node.cs
@@ -293,7 +293,9 @@ namespace DLT.Meta
             Logging.trace("Last five signature counts:");
             for (int i = 0; i < 6; i++)
             {
-                Logging.trace(String.Format(" -> block #{0}, signatures: {1}.", Node.blockChain.getLastBlockNum() - (ulong)i, Node.blockChain.getBlockSignaturesReverse(i)));
+                ulong blockNum = Node.blockChain.getLastBlockNum() - (ulong)i;
+                Block block = Node.blockChain.getBlock(blockNum);
+                Logging.trace(String.Format(" -> block #{0}, signatures: {1}, checksum: {2}, wsChecksum: {3}.", blockNum, Node.blockChain.getBlockSignaturesReverse(i), block.blockChecksum, block.walletStateChecksum));
             }
             Logging.trace(String.Format(" -> Block processor is operating: {0}.", Node.blockProcessor.operating));
             Logging.trace(String.Format(" -> Block processor is synchronizing: {0}.", Node.blockSync.synchronizing));
