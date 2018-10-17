03a3fece82fb77444c9756b82257d6974cf3e2e0
Cristian Ciorba <cristian@firestorm.ro>
2018-10-18 02:46:36 +0300

    - fix for low balance signatures

--- IxianDLT/Block/BlockProcessor.cs
+++ IxianDLT/Block/BlockProcessor.cs
@@ -140,7 +140,7 @@ namespace DLT
                 }
 
                 Presence p = null;
-                if (Legacy.isLegacy(b.blockNum))
+                if (Legacy.isLegacy(b.blockNum) || parts[1].Length < 70)
                 {
                     p = PresenceList.presences.Find(x => x.metadata == parts[1]);
                 }
@@ -187,7 +187,8 @@ namespace DLT
                 }
 
                 string address = parts[1];
-                if (Legacy.isLegacy(b.blockNum))
+
+                if (Legacy.isLegacy(b.blockNum) || address.Length < 70)
                 {
                     address = (new Address(parts[1])).ToString();
                 }
