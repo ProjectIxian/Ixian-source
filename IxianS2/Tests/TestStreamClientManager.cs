c1b5f8d524a41cdbf40a38b0cc5f1dc9ed2a5c7e
Cristian Ciorba <cristian@firestorm.ro>
2018-10-27 14:46:38 +0300

    - added connectTo support for TestStreamClientManager

--- IxianS2/Tests/TestStreamClientManager.cs
+++ IxianS2/Tests/TestStreamClientManager.cs
@@ -1,5 +1,6 @@
 ﻿using DLT;
 using DLT.Meta;
+using DLT.Network;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -22,13 +23,14 @@ namespace S2
         {
             streamClients = new List<TestStreamClient>();
 
-            if(Config.testS2Node.Length < 3)
+            if (Config.testS2Node.Length < 3)
             {
                 Logging.error("Invalid S2 node for Test Client. Use -n or -s2node to specify node.");
                 Node.running = false;
                 S2.Program.noStart = true;
             }
 
+            connectTo(Config.testS2Node);
 
             // Start the reconnect thread
             reconnectThread = new Thread(reconnectClients);
@@ -80,6 +82,114 @@ namespace S2
 
         }
 
+        // Connects to a specified node, with the syntax host:port
+        public static bool connectTo(string host)
+        {
+            Logging.info(String.Format("Connecting to S2 node: {0}", host));
 
+            if (host == null || host.Length < 3)
+            {
+                Logging.error(String.Format("Invalid host address {0}", host));
+                return false;
+            }
+
+            string[] server = host.Split(':');
+            if (server.Count() < 2)
+            {
+                Logging.warn(string.Format("Cannot connect to invalid hostname: {0}", host));
+                return false;
+            }
+
+            // Resolve the hostname first
+            string resolved_server_name = NetworkUtils.resolveHostname(server[0]);
+
+            // Skip hostnames we can't resolve
+            if (resolved_server_name.Length < 1)
+            {
+                Logging.warn(string.Format("Cannot resolve IP for {0}, skipping connection.", server[0]));
+                return false;
+            }
+
+            string resolved_host = string.Format("{0}:{1}", resolved_server_name, server[1]);
+
+            // Verify against the publicly disclosed ip
+            // Don't connect to self
+            if (resolved_server_name.Equals(Config.publicServerIP, StringComparison.Ordinal))
+            {
+                if (server[1].Equals(string.Format("{0}", Config.serverPort), StringComparison.Ordinal))
+                {
+                    Logging.info(string.Format("Skipping connection to public self seed node {0}", host));
+                    return false;
+                }
+            }
+
+            // Get all self addresses and run through them
+            List<string> self_addresses = CoreNetworkUtils.GetAllLocalIPAddresses();
+            foreach (string self_address in self_addresses)
+            {
+                // Don't connect to self
+                if (resolved_server_name.Equals(self_address, StringComparison.Ordinal))
+                {
+                    if (server[1].Equals(string.Format("{0}", Config.serverPort), StringComparison.Ordinal))
+                    {
+                        Logging.info(string.Format("Skipping connection to self seed node {0}", host));
+                        return false;
+                    }
+                }
+            }
+
+            lock (connectingClients)
+            {
+                foreach (string client in connectingClients)
+                {
+                    if (resolved_host.Equals(client, StringComparison.Ordinal))
+                    {
+                        // We're already connecting to this client
+                        return false;
+                    }
+                }
+
+                // The the client to the connecting clients list
+                connectingClients.Add(resolved_host);
+            }
+
+            // Check if node is already in the client list
+            lock (streamClients)
+            {
+                foreach (TestStreamClient client in streamClients)
+                {
+                    if (client.getFullAddress(true).Equals(resolved_host, StringComparison.Ordinal))
+                    {
+                        // Address is already in the client list
+                        return false;
+                    }
+                }
+            }
+
+
+            // Connect to the specified node
+            TestStreamClient new_client = new TestStreamClient();
+            // Recompose the connection address from the resolved IP and the original port
+            bool result = new_client.connectToServer(resolved_server_name, Convert.ToInt32(server[1]));
+
+            // Add this node to the client list if connection was successfull
+            if (result == true)
+            {
+                // Add this node to the client list
+                lock (streamClients)
+                {
+                    streamClients.Add(new_client);
+                }
+
+            }
+
+            // Remove this node from the connecting clients list
+            lock (connectingClients)
+            {
+                connectingClients.Remove(resolved_host);
+            }
+
+            return result;
+        }
     }
 }
