c2b42dccba09c68d8cdb57785593eaf378a2b0c9
IxiAngel <no-reply@ixian.io>
2019-03-28 05:23:56 +0100

    xdc-0.6.3d - improved processHelloMessage

--- IxianDLT/Meta/Config.cs
+++ IxianDLT/Meta/Config.cs
@@ -52,7 +52,7 @@ namespace DLT
             public static string externalIp = "";
 
             // Read-only values
-            public static readonly string version = "xdc-0.6.3d-dev"; // DLT Node version
+            public static readonly string version = "xdc-0.6.3d"; // DLT Node version
             public static bool isTestNet = false; // Testnet designator
 
             public static readonly ulong maxBlocksPerDatabase = 1000;
