32499d9d1c78232efd8a6f17010754ae3121ced1
IxiAngel <no-reply@ixian.io>
2019-04-22 17:44:01 +0100

    xdc-0.6.3f

--- IxianDLT/Meta/Config.cs
+++ IxianDLT/Meta/Config.cs
@@ -53,7 +53,7 @@ namespace DLT
             public static string externalIp = "";
 
             // Read-only values
-            public static readonly string version = "xdc-0.6.3f-dev"; // DLT Node version
+            public static readonly string version = "xdc-0.6.3f"; // DLT Node version
             public static bool isTestNet = false; // Testnet designator
 
             public static readonly ulong maxBlocksPerDatabase = 1000;
